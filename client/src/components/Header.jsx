e1f31a87f3f3846eb5e7fc4bb2c8bf740495260e
ibrahim mulei <ibrahimmulei@gmail.com>
2024-07-09 02:39:32 +0300

    new edit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,21 +17,21 @@ export default function Header() {
             type="text"
             placeholder="Search...."
           />
-          <FaSearch className="text-slate-500" />
+          <FaSearch className="text-slate-500 hover:text-slate-700" />
         </form>
         <ul className="flex gap-3 p-1">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-500">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-500">
               About
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className=" sm:inline text-slate-700 hover:underline">
+            <li className="p-1.5 w-30 bg-green-700 rounded-md sm:inline text-white hover:bg-green-600">
               Sign-in
             </li>
           </Link>
