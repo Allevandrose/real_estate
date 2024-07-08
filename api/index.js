bf214be673bcef3bda4ea2d8cd62ff3c456e67aa
ibrahim mulei <ibrahimmulei@gmail.com>
2024-07-08 15:30:29 +0300

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB successfuly!!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(5000, () => {
