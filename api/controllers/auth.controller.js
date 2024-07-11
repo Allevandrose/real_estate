98d2b1d19d766c8a417394d856adbedfbd7ac635
ibrahim mulei <ibrahimmulei@gmail.com>
2024-07-11 12:14:17 +0300

    added a sign in route added its data to the database using (POST) handled its errors using try-catch block, which is a way to handle errors in JavaScript

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,1 +1,8 @@
-export const signup = (req, res) => {};
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+};
